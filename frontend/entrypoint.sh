46c2e66c9bd97176f3fc1a48e2919db821e64575
Rakesh Reddy <rakeshreddyk6312@outlook.com>
2026-09-09 12:00:43 +0530

    Fix: use /etc/nginx/ instead of /tmp/ (Render clears /tmp at startup)

--- frontend/entrypoint.sh
+++ frontend/entrypoint.sh
@@ -3,7 +3,7 @@
 set -e
 
 envsubst '${PORT} ${BACKEND_URL} ${BACKEND_HOST}' \
-  < /tmp/nginx.conf.template \
+  < /etc/nginx/nginx.conf.template \
   > /etc/nginx/conf.d/default.conf
 
 exec nginx -g "daemon off;"
